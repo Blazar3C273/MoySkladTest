7d2b87d151da59ba066c52f765d7d2caf56d4774
Anatolii Stepanenko <tolik.stepanenko@gmail.com>
2015-11-24 16:54:40 +0300

    All working. Final version

--- src/main/java/GraphUtils.java
+++ src/main/java/GraphUtils.java
@@ -1,38 +1,72 @@
-import org.jgrapht.alg.cycle.JohnsonSimpleCycles;
 import org.jgrapht.alg.cycle.TiernanSimpleCycles;
 import org.jgrapht.graph.DefaultDirectedGraph;
 import org.jgrapht.graph.DefaultEdge;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
 public class GraphUtils {
-    public DefaultDirectedGraph<Integer, DefaultEdge> graph;
+    private DefaultDirectedGraph<Integer, DefaultEdge> graph;
 
+    public DefaultDirectedGraph<Integer, DefaultEdge> getGraph() {
+        return graph;
+    }
+    //Патерн, по которому проверяется валидность строк. Невалидные строки игнорируются.
     private static final Pattern pattern = Pattern.compile("(\\d+)\\s(\\d+)$");
+
+    public static void main(String[] args) {
+        try {
+            //Читаем имя файла из stdin. Тут столкнулся с не точной формулировкой задания.
+            //В ней сказано "файл читать из stdin". непонятно что именно читать. Имя файла? Полный путь? Содержимое?
+            //Если все таки содержимое то при чем здесь файл? Тогда это просто чтение даных из stdin.
+            //если все таки последний вариант корректный, то создание экземпляра выгладит так:
+            // GraphUtils graphUtils = new GraphUtils(System.in);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            GraphUtils graphUtils = new GraphUtils(Files.newInputStream(Paths.get(br.readLine())));
+
+            for (List<Integer> integers : graphUtils.getLoops()) {
+                for (Integer integer : integers) {
+                    System.out.print(integer + " ");
+                }
+                System.out.println("");
+            }
+
+        } catch (IOException e) {
+            if (e.getClass().equals(NoSuchFileException.class))
+                System.out.println("No file with name \"" + e.getMessage() + "\".");
+        }
+    }
 
     public GraphUtils(InputStream inputStream) {
         graph = new DefaultDirectedGraph<>(DefaultEdge.class);
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-        //каждую строку разбить на 2 числа и этими числами заинитить граф
-        br.lines().filter(GraphUtils::isValidString).forEach(s -> addVertexByString(s,graph));
+        //отсеять невалидные строки
+        //каждую строку разбить на 2 числа и этими числами построить граф
+        br.lines().filter(GraphUtils::isValidString).forEach(s -> addVertexByString(s, graph));
     }
 
     private static void addVertexByString(String s, DefaultDirectedGraph<Integer, DefaultEdge> graph) {
-        Integer v1,v2,splitterIndex;
+        Integer v1, v2, splitterIndex;
         splitterIndex = s.indexOf(' ');
-        v1 = Integer.valueOf(s.substring(0,splitterIndex));
-        v2 = Integer.valueOf(s.substring(splitterIndex+1));
+        v1 = Integer.valueOf(s.substring(0, splitterIndex));
+        v2 = Integer.valueOf(s.substring(splitterIndex + 1));
+
         if (!graph.containsVertex(v1))
             graph.addVertex(v1);
         if (!graph.containsVertex(v2))
             graph.addVertex(v2);
-        graph.addEdge(v1,v2);
+
+        graph.addEdge(v1, v2);
 
     }
 
@@ -46,7 +80,7 @@ public class GraphUtils {
     @Override
     public boolean equals(Object obj) {
         if (obj != null && obj.getClass().equals(GraphUtils.class))
-            return graph.equals(((GraphUtils)obj).graph);
+            return graph.equals(((GraphUtils) obj).graph);
         return false;
     }
 
@@ -56,8 +90,12 @@ public class GraphUtils {
 
     public List<List<Integer>> getLoops() {
         TiernanSimpleCycles tsc = new TiernanSimpleCycles(graph);
-        return tsc.findSimpleCycles();
 
+        List<List<Integer>> cycles = (List<List<Integer>>) tsc.findSimpleCycles();
+        for (List<Integer> cycle : cycles) {
+            cycle.add(cycle.get(0));
+        }
+        return cycles;
     }
 
     @Override
@@ -66,4 +104,6 @@ public class GraphUtils {
                 "graph=" + graph +
                 '}';
     }
+
+
 }
--- src/test/java/MainTestSuit.java
+++ src/test/java/MainTestSuit.java
@@ -1,76 +1,67 @@
 import org.jgrapht.DirectedGraph;
 import org.jgrapht.graph.DefaultDirectedGraph;
-import org.jgrapht.graph.DefaultEdge;
 import org.junit.Test;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 /**
- * Created by terron on 23.11.15.
+ * @author Stepanenko Anatolii
+ * @version 0.1
+ * Created on 24.11.15
  */
 public class MainTestSuit {
     private GraphUtils graphUtils = new GraphUtils();
 
-    @Test
-    public void compareFirstLoop() throws Exception {
-        int[] firstLoop = LoopFinder.findLoops(graphUtils).get(0);
-        assertArrayEquals("arrays must be equals", firstLoop, new int[]{1,2,1});
-    }
 
     @Test
     public void graphIsLoadingFromFile() throws Exception {
-        GraphUtils graphUtils = new GraphUtils(Files.newInputStream(Paths.get("./src/test/resources/input.txt")));
+        GraphUtils graphUtils = new GraphUtils(Files.newInputStream(Paths.get("./src/test/resources/input2.txt")));
         GraphUtils expectedGraphUtils = new GraphUtils();
-        expectedGraphUtils.graph.addVertex(1);
-        expectedGraphUtils.graph.addVertex(2);
-        expectedGraphUtils.graph.addVertex(3);
-        expectedGraphUtils.graph.addVertex(4);
-        expectedGraphUtils.graph.addVertex(5);
-        expectedGraphUtils.graph.addVertex(6);
-        //expectedGraphUtils.graph.addVertex(7);
-        //expectedGraphUtils.graph.addVertex(8);
+        expectedGraphUtils.getGraph().addVertex(1);
+        expectedGraphUtils.getGraph().addVertex(2);
+        expectedGraphUtils.getGraph().addVertex(3);
+        expectedGraphUtils.getGraph().addVertex(4);
+        expectedGraphUtils.getGraph().addVertex(5);
+        expectedGraphUtils.getGraph().addVertex(6);
+        //expectedGraphUtils.getGraph().addVertex(7);
+        //expectedGraphUtils.getGraph().addVertex(8);
 
-        expectedGraphUtils.graph.addEdge(1,2);
-        expectedGraphUtils.graph.addEdge(2,1);
-        expectedGraphUtils.graph.addEdge(3,4);
-        expectedGraphUtils.graph.addEdge(5,6);
-        expectedGraphUtils.graph.addEdge(6,5);
-        //expectedGraphUtils.graph.addEdge(7,8);
-        //expectedGraphUtils.graph.addEdge(8,5);
+        expectedGraphUtils.getGraph().addEdge(1, 2);
+        expectedGraphUtils.getGraph().addEdge(2, 1);
+        expectedGraphUtils.getGraph().addEdge(3, 4);
+        expectedGraphUtils.getGraph().addEdge(5, 6);
+        expectedGraphUtils.getGraph().addEdge(6, 5);
+        //expectedGraphUtils.getGraph().addEdge(7,8);
+        //expectedGraphUtils.getGraph().addEdge(8,5);
 
         //charset? another characters
         //bigInteger
         //направление зависимостей
-        System.out.println("expectedGraphUtils = " + expectedGraphUtils.graph);
-        System.out.println("graphUtils = " + graphUtils.graph);
+        System.out.println("expectedGraphUtils = " + expectedGraphUtils.getGraph());
+        System.out.println("graphUtils = " + graphUtils.getGraph());
         System.out.println(graphUtils.getLoops());
         assertEquals("graphUtils must be the same", expectedGraphUtils, graphUtils);
     }
 
     @Test
-    public void doStuff() throws Exception {
-        DirectedGraph<Integer,DefaultEdge> graph = new DefaultDirectedGraph<Integer,DefaultEdge>(DefaultEdge.class);
-
-    }
-
-    @Test
     public void graphComparation() throws Exception {
-        DirectedGraph<Integer,Integer> graph1,graph2,graph3;
-        graph1 = new DefaultDirectedGraph<Integer, Integer>(Integer.class);
-        graph2 = new DefaultDirectedGraph<Integer, Integer>(Integer.class);
-        graph3 = new DefaultDirectedGraph<Integer, Integer>(Integer.class);
+        DirectedGraph<Integer, Integer> graph1, graph2, graph3;
+        graph1 = new DefaultDirectedGraph<>(Integer.class);
+        graph2 = new DefaultDirectedGraph<>(Integer.class);
+        graph3 = new DefaultDirectedGraph<>(Integer.class);
 
         for (int i = 0; i < 10; i++) {
             graph1.addVertex(i);
             graph2.addVertex(i);
         }
 
-        assertNotEquals(graph1,null);
-        assertNotEquals(graph1,graph3);
-        assertEquals("graphs must be equals", graph1,graph2);
+        assertNotEquals(graph1, null);
+        assertNotEquals(graph1, graph3);
+        assertEquals("graphs must be equals", graph1, graph2);
 
     }
 }
